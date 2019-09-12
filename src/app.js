e82a7da71ecedca7bafbbc0bebe102e4db920c3c
clalil <cliljander@gmail.com>
2019-09-12 10:06:29 +0200

    Add styling for cards using Semantic UI, cards are placed as grids but doubles instead of singles, all tests passing

--- src/app.js
+++ src/app.js
@@ -9,20 +9,77 @@ const renderContacts = () => {
 
    contacts.forEach(contact => {
       let li = document.createElement('li')
-      li.innerHTML = `
-      <div class="card">
-        <div class="image">
-          <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
-        </div>
-        <div class="content">
-          <h1>${ contact.name }</h1>
-          <h2>${ contact.company }</h2>
-          <p>${ contact.notes }</p>
-          <p>${ contact.email } |
-          <a href="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a></p>
-        < /div>
-      </div>
+      li.innerHTML = 
       `
+<div class="ui grid">
+  <div class="four wide column">
+          <div class="ui card">
+          <div class="image">
+            <img src="https://ca-address-book.herokuapp.com/images/pine.jpg">
+          </div>
+          <div class="content">
+            <a class="header">${ contact.name }</a>
+            <div class="meta">
+              <span class="date">Joined in 2013</span>
+            </div>
+            <div class="description">
+            <h2>${ contact.company }</h2>
+                <p>${ contact.notes }</p>
+                <p>${ contact.email } |
+                <a href="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a></p>
+            </div>
+          </div>
+          <div class="extra content">
+              <a>
+                <i class="user icon"></i>
+                22 Friends
+              </a>
+            </div>
+          </div>
+  </div>
+  <div class="four wide column">
+                    <div class="ui card">
+                    <div class="image">
+                      <img src="https://ca-address-book.herokuapp.com/images/pine.jpg">
+                    </div>
+                    <div class="content">
+                      <a class="header">${ contact.name }</a>
+                      <div class="meta">
+                        <span class="date">Joined in 2013</span>
+                      </div>
+                      <div class="description">
+                      <h2>${ contact.company }</h2>
+                          <p>${ contact.notes }</p>
+                          <p>${ contact.email } |
+                          <a href="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a></p>
+                      </div>
+                    </div>
+                    <div class="extra content">
+                        <a>
+                          <i class="user icon"></i>
+                          22 Friends
+                        </a>
+                      </div>
+                    </div>
+  </div>
+</div>
+`
+ 
+  // <div class="four wide column"></div>
+  // <div class="four wide column"></div> 
+      // <div class="card">
+      //   <div class="image">
+      //     <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
+      //   </div>
+      //   <div class="content">
+      //     <h1>${ contact.name }</h1>
+      //     <h2>${ contact.company }</h2>
+      //     <p>${ contact.notes }</p>
+      //     <p>${ contact.email } |
+      //     <a href="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a></p>
+      //   < /div>
+      // </div>
+
       ul.appendChild(li)  
     })
     div.appendChild(ul)
